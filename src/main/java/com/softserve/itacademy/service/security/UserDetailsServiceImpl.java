ea29ff476228c412283adcb759488d0db165ea63
Angela Matiiva <angelamatiiva@gmail.com>
2023-01-17 00:42:15 +0200

    first commit

---
+++ src/main/java/com/softserve/itacademy/service/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.softserve.itacademy.service.security;
+
+import com.softserve.itacademy.model.User;
+import com.softserve.itacademy.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("The user with this user name does not exists");
+        }
+        return new MyUserDetails(user);
+    }
+}
