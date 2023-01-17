aaff8d2fb851acec75020e000d9386deb7ce762a
Olesia <olesjayakovenko@gmail.com>
2023-01-17 18:14:16 +0200

    Set a redirect to a link with ToDo lists when authentication is successful

--- src/main/java/com/softserve/itacademy/config/SecurityConfig.java
+++ src/main/java/com/softserve/itacademy/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -34,27 +35,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(authenticationProvider());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/users/create").permitAll()
+                .antMatchers("/tasks/**").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers("/todos/**").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers("/users/**").hasAnyAuthority("USER", "ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login-form")
                 .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/home")
+                .defaultSuccessUrl("/", true)
                 .failureUrl("/login-form?error=true")
                 .permitAll()
                 .and()
                 .logout()
                 .logoutUrl("/perform-logout")
                 .logoutSuccessUrl("/login-form")
-                .deleteCookies("JSESSIONID")
-                .and()
-                .csrf().disable();
+                .deleteCookies("JSESSIONID");
     }
 }
--- src/main/java/com/softserve/itacademy/controller/HomeController.java
+++ src/main/java/com/softserve/itacademy/controller/HomeController.java
@@ -1,7 +1,8 @@
 package com.softserve.itacademy.controller;
 
-import com.softserve.itacademy.service.RoleService;
 import com.softserve.itacademy.service.UserService;
+import com.softserve.itacademy.service.security.MyUserDetails;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     private final UserService userService;
+
     public HomeController(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping({"/", "home"})
-    public String home(Model model) {
+    public String home(Model model, Authentication auth) {
+        MyUserDetails userDetails = (MyUserDetails) auth.getPrincipal();
+        if (!userDetails.getUserAuthority().equals("ADMIN")) {
+            return "redirect:/todos/all/users/" + userDetails.getUserId();
+        }
         model.addAttribute("users", userService.getAll());
         return "home";
     }
